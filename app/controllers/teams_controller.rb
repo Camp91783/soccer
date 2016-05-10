c6dfe74d399a65cdfcdec5732a8804e51283f2c1
Peter Camp <Pete.f.Camp@gmail.com>
2016-05-10 11:06:37 -0500

    adding in views for teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,25 +1,47 @@
 class TeamsController < ApplicationController
+  
+  def index
+    @teams=Teams.all 
+
+  end
+
   def new
     @team = Team.new
   end
 
-  def edit
-  end
 
   def create
-  end
-
-  def update
+      @team = Team.new(team_params)
+    if @team.save
+      redirect_to :Teams, notice: "Success"
+    else
+      flash.now[:alert] = "Couldn't save"
+      render :new
   end
 
   def show
+    @team = Team.find(params[:id])
   end
 
-  def destroy
+  def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(team_params)
+      redirect_to team_path(@wolf), notice: "Successfully Updated!!!!"
+    else
+      flash.now[:alert]="Couldn't update."
+      render :edit
+    end
   end
 
-  def index
-
-
+  def edit
+    @team = Team.find(params[:id])
   end
+
+ def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to :teams, notice: "Successfully deleted!!!"
+  end
+
+  
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,3 @@
 class Team < ActiveRecord::Base
-	t.string = :name
 	
 end
