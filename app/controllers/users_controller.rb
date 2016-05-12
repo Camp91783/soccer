dd5be88be8dca98c6dcb6a1184de20d6e78d4c7f
Peter Camp <Pete.f.Camp@gmail.com>
2016-05-12 18:42:21 -0500

    trying to fix log in route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   		if @user.save
-  			redirect_to user_path, notice:  "Saved"
+  			redirect_to :league, notice:  "Saved"
   		else
   			render :new
   		end
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
 private 
 	def user_params
-		params.require(:user). permit(:email, 
+		params.require(:user).permit(:email, 
 			:password, :password_confirmation)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  validates_confirmation_of :password
 end
