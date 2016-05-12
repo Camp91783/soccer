8fe0c446624e29bf4b5cfc9682f0a1cc9a7a36ae
Peter Camp <Pete.f.Camp@gmail.com>
2016-05-12 11:33:35 -0500

    added league migration

--- db/migrate/20160510200611_create_players.rb
+++ db/migrate/20160510200611_create_players.rb
@@ -3,7 +3,7 @@ class CreatePlayers < ActiveRecord::Migration
     create_table :players do |t|
       t.string :name
       t.string :email
-      t.belongs_to :team 
+      t.belongs_to :teams 
 
       t.timestamps null: false
     end
---
+++ db/migrate/20160512163015_create_league_table.rb
@@ -0,0 +1,6 @@
+class CreateLeagueTable < ActiveRecord::Migration
+  def change
+    create_table :league_tables do |t|
+    end
+  end
+end
