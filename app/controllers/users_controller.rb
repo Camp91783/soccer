36ad2a3fd8d145a2e64b6c95f7828c7f1280e2a3
Peter Camp <Pete.f.Camp@gmail.com>
2016-05-12 17:25:02 -0500

    fixing password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,30 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
 
+  def create
+  	@users = User.new(user_params)
+  		if user.save
+  			redirect_to users_path, notice:  "Saved"
+  		else
+  			render :new
+  		end
+  end 
+
   def show
+  	@user = User.find(params[:id])
   end
 
   def index
+  	@user = User.all
   end
-end
+
 
 private 
 	def user_params
-		params_require(:user).permit(:email, :password, :password_confirmation)
+		params.require(:user). permit(:email, 
+			:password, :password_confirmation)
 	end
 end
+
