a2dc114cfa2232682b178ae787148afe81cb0d46
Peter Camp <Pete.f.Camp@gmail.com>
2016-05-13 15:13:29 -0500

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+
+ def index
+    @players = Player.all
+    @league=League.all 
+    @teams=Team.all 
+    @user = User.all
+ end
+
+ 
